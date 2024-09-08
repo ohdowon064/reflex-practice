d4bba0ca897d8dc6a2a1c5025a839f04a09250ee
Dowon Oh <dhehdnjs03@gmail.com>
2024-09-09 00:04:40 +0900

    폼 데이터 처리

--- reflex_practice/pages/contact.py
+++ reflex_practice/pages/contact.py
@@ -4,15 +4,42 @@ from reflex_practice import navigation
 from reflex_practice.ui.base import base_page
 
 
+class ContactState(rx.State):
+    form_data: dict = {}
+
+    def handle_submit(self, form_data: dict):
+        self.form_data = form_data
+        print(self.form_data)
+
+
 @rx.page(route=navigation.routes.CONTACT_ROUTE)
 def contact_page() -> rx.Component:
-    return base_page(
+    my_form = rx.form(
         rx.vstack(
-            rx.heading("Contact Us", size="9"),
-            spacing="5",
-            justify="center",
-            align="center",
-            min_height="85vh",
-            id="my-child",
-        )
+            rx.input(
+                placeholder="First Name",
+                name="first_name",
+                required=True,
+            ),
+            rx.input(
+                placeholder="Last Name",
+                name="last_name",
+                required=True,
+            ),
+            rx.input(
+                placeholder="Email",
+                name="email",
+                type="email",
+                required=True,
+            ),
+            rx.text_area(
+                placeholder="Message",
+                name="message",
+                required=True,
+            ),
+            rx.button("Submit", type="submit"),
+        ),
+        on_submit=ContactState.handle_submit,
+        reset_on_submit=True,
     )
+    return base_page(my_form)
